f272dd5c113cb67eb4a1235f30f6bb3e80785975
Sonu Jose <sonu.j@gj.com>
2019-06-27 15:56:29 +0530

    first commit of newgitexp

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace newgitexp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
