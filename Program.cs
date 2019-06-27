2b4f2f45949544f224d39599a83b4875b957f6ac
Sonu Jose <sonu.j@gj.com>
2019-06-27 16:02:32 +0530

    "sonu commits"

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace newgitexp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("sonumol");
         }
     }
 }
