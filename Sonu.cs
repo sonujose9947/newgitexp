22943a9b639a33a508d7e7637a7bbffe137d8848
Nimmy Johnson <nimmy.j@gj.com>
2019-06-27 17:25:36 +0530

    nimmy Commit Finally

--- Sonu.cs
+++ Sonu.cs
@@ -2,6 +2,9 @@ namespace newgitexp
 {
     public class Sonu
     {
+        private string name;
+        private int id;
+
         public string Name { get => name; set => name = value; }
         public int Id { get => id; set => id = value; }
     }
