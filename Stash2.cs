1d915263cd40cf897efa476b506577cb021bd516
Sonu Jose <sonu.j@gj.com>
2019-06-28 15:59:43 +0530

    sonu commit stash

---
+++ Stash2.cs
@@ -0,0 +1,10 @@
+namespace newgitexp
+{
+    class Stash2{
+        int length=0;
+        int width=0;
+
+        public int Length { get => length; set => length = value; }
+        public int Width1 { get => width; set => width = value; }
+    }
+}
